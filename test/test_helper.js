ef5d7701ab6f1a65fb562e218c652ebc715c1fbd
1001b <10011111b@gmail.com>
2015-12-24 22:24:34 +0800

    use jsdom-no-contextify

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,17 +1,23 @@
 /**
  * Created by jeffreylaw on 12/10/15.
  */
-import jsdom from 'jsdom';
+
+import jsdom from 'jsdom-no-contextify'
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
+
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
 
 global.document = doc;
 global.window = win;
+global.navigator = {
+  userAgent: 'node.js'
+};
 
 Object.keys(window).forEach((key) => {
+  console.log(key);
   if (!(key in global)) {
     global[key] = window[key];
   }
